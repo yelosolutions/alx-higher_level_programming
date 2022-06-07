33449be4efd20557a5d1d9f3fa2bb2d6359004b7
yelosolutions <yelosolutions69@gmail.com>
2022-06-07 00:51:06 +0000

    prints some basic info about python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_python_list_info - prints basic info about python lists.
+ *
+ * p: python list
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyObject *list_len;
+	PyObject *list_sum;
+	PyObject *list_max;
+	PyObject *list_min;
+	PyObject *list_avg;
+	PyObject *list_rev;
+	PyObject *list_pal;
+	PyObject *list_rev_pal;
+	PyObject *list_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_ rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+	PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+}
