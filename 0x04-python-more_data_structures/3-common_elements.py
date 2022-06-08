23d8cd4a1ed333c82082ab16df191e50ae0b603a
yelosolutions <yelosolutions69@gmail.com>
2022-06-08 19:11:18 +0000

    returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1=[], set_2=[]):
+    if set_1 is None or len(set_1) == 0:
+        return set_1
+    if set_2 is None or len(set_2) == 0:
+        return set_2
+
+    return set_1 & set_2
