6a45b916c7cdb8faa55fa4511b6884383927761b
yelosolutions <yelosolutions69@gmail.com>
2022-06-20 21:13:44 +0000

    101-safe_function.py file commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    """
+    Excecute a function safely
+
+    Args:
+        fct: the function to be executed
+        args: arguments for fct
+
+    Return: the return value of the fct if succesful. Otherwise null
+    """
+    try:
+        return fct(*args)
+    except BaseExceptions as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
