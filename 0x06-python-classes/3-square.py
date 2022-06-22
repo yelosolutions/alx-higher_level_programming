82029c204153beb4e0a9fa864fdea74dcbd8d6af
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-06-22 05:12:36 +0300

    task 3 commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,6 @@ class Square:
     def __init__(self, size=0):
         """
         Creates an instance of Square
-
         Args:
             size: size of the square
         """
@@ -16,12 +15,12 @@ class Square:
             raise TypeError("size must be an integer")
 
         if size < 0:
-            raise ValueError("size must be => 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = size
 
     def area(self):
         """
-        Finds area of a square
+        Finds area of the square
         """
-        return self.__size * self._size
+        return self.__size * self.__size
