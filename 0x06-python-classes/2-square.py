b1cb63c16e0524ad1a9322f344163818c818612a
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-06-22 05:13:45 +0300

    task 2 commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,13 +2,15 @@
 """2-square.py"""
 
 
+from wsgiref.simple_server import WSGIRequestHandler
+
+
 class Square:
     """Defines a square"""
 
     def __init__(self, size=0):
         """
         Creates an instance of Square
-
         Args:
             size: size of the square
         """
@@ -16,6 +18,6 @@ class Square:
             raise TypeError("size must be an integer")
 
         if size < 0:
-            raise ValueError("size must be => 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = size
