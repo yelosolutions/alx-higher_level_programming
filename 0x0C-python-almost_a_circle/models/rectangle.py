3f72b035b7f3d19753dad346980e2babec051d2f
yelosolutions <ronokamau@gmail.com>
2022-07-11 21:41:09 +0000

    second corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/python3
 """ Module that contains class Rectangle,
 inheritance of class Base
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/python3
 """ Module that contains class Square,
 inheritance of class Rectangle
