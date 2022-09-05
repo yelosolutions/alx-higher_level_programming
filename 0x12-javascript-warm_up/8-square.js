cdfc9afb1b963c76b2229ccd6cedcef5dbed36d5
yelosolutions <ronokamau@gmail.com>
2022-09-05 19:29:09 +0000

    Double space instead of tab commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (Number.isNaN(size)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let i = 0, k; i < size; i++) {
-		k = '';
-		for (let j = 0; j < size; j++) {
-			k += 'X';
-		}
-		console.log(k);
-	}
+  for (let i = 0, k; i < size; i++) {
+    k = '';
+    for (let j = 0; j < size; j++) {
+      k += 'X';
+    }
+    console.log(k);
+  }
 }
