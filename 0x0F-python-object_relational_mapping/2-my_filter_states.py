798c177037b409683b05403064d23cdceb6d7498
yelosolutions <yelosolutions69@gmail.com>
2022-09-11 13:33:24 +0000

    Task 2 commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,14 +6,14 @@ matches the argument
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user="root", passwd="1914Jr@!",
-            db="hbtn_0e_0_usa":)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     values = db.cursor()
     values.execute("SELECT * \
-            FROM states \
-            WHERE CONVERT(`name` USING Latin1) \
-            COLLATE Latin1_General_CS = '{}';")
+                    FROM states \
+                    WHERE CONVERT(`name` USING Latin1) \
+                    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
     states = values.fetchall()
 
     for state in states:
