8b9e3e7026d37bb2b10772a264e805609eddb0a9
yelosolutions <yelosolutions69@gmail.com>
2022-09-11 13:37:20 +0000

    Task 2 commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd="1914Jr@!",
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
     c = db.cursor()
